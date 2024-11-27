c57d6e8c7eacb7c41f3dbbfa293dcbf9856e6014
hvgh88 <harshita.vidapanakal@gmail.com>
2024-11-26 22:24:11 -0500

    minor fix

--- src/main/java/com/umass/hangout/controller/HealthCheckController.java
+++ src/main/java/com/umass/hangout/controller/HealthCheckController.java
@@ -1,5 +1,9 @@
 package com.umass.hangout.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HealthCheckController {
     @GetMapping("/")
